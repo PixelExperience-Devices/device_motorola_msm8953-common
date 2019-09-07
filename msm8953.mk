75cbd48ef87e110279d020dbae5981b29055736b
Erfan Abdi <erfangplus@gmail.com>
2019-09-07 17:47:30 +0430

    msm8953-common: Wrap libgui_vendor into libwui
    Change-Id: Ifa25ec6a2aaf3855c24b2a3a46b80abb8501b189
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libgui_vendor \
+    libwui \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     MotCamera2
