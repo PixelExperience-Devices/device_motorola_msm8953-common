88f5c17a4ab0082f3e8309cadca2d5e248f9ac6c
LuK1337 <priv.luk@gmail.com>
2020-10-19 21:42:02 +0200

    msm8953-common: TetheringOverlay: Disable BPF
    * Such is life with 3.18 kernels.
    
    Change-Id: I1c5e3fab63271b1e3fe6c814d96edbde782f27e8

--- msm8953.mk
+++ msm8953.mk
@@ -348,6 +348,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(COMMON_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
