c274deb5fb6c9899cf090bd8c8218ed3a6f7643f
jeangraff30 <jeangraff30@gmail.com>
2020-12-22 01:03:15 -0300

    msm8953-common: decomonize camera@1.0-impl
    Signed-off-by: jeangraff30 <jeangraff30@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -76,7 +76,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
     camera.device@3.2-impl \
     libwui \
     vendor.qti.hardware.camera.device@1.0 \
