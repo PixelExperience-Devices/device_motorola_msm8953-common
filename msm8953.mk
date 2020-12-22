8351fcaa10226242c0410888a3802f7dd37e0d11
jeangraff30 <jeangraff30@gmail.com>
2020-12-22 10:03:23 -0300

    msm8953-common: switch to Camera2

--- msm8953.mk
+++ msm8953.mk
@@ -77,10 +77,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
+    Camera2 \
     libwui \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    MotCamera2
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
