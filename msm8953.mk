ee2a089dffcc5c478e69335df13b05312c96b5ce
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    msm8953-common: Switch to source built vendor.qti.hardware.perf@2.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    What's worse, the minimal CPU frequency of both clusters will be locked
    at 1036MHz on msm8953.
    
    Change-Id: I1dc96e4a51c4c81dd847c59a58ec670d89054f64

--- msm8953.mk
+++ msm8953.mk
@@ -245,7 +245,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Powerhint
 PRODUCT_COPY_FILES += \
